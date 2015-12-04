922da89e536875828a49d8c3ce27467f0da962a4
jwygralak67 <forty45@f813a028846a>
2015-12-04 04:15:56 +0000

    There is safety in numbers.

---
+++ darbot002.py
@@ -0,0 +1,30 @@
+import rg
+
+class Robot:
+    def act(self, game):
+        
+        print
+        print "Turn: " +str( game.turn) + "\tBot: " + str(self.robot_id)
+        # find closest friend & enemy
+        closestFriend = (1000, 1000)
+        closestEnemy = (1000, 1000)
+        for loc, bot in game.get('robots').items():
+            if bot.player_id != self.player_id:
+                if rg.wdist(loc, self.location) <= rg.wdist(closestEnemy, self.location):
+                    closestEnemy = loc
+            else:
+                if rg.wdist(loc, self.location) <= rg.wdist(closestFriend, self.location) and self.robot_id != bot.robot_id:
+                    closestFriend = loc
+        
+        for loc, bot in game['robots'].iteritems():
+            # if there are enemies around, attack them
+            if bot.player_id != self.player_id:
+                if rg.dist(loc, self.location) <= 1:
+                    return ['attack', loc]
+            else:
+                # if there are friends around, move to them
+                if rg.dist(loc, self.location) > 2:
+                    return ['move', rg.toward(self.location, closestFriend)]
+                    
+        # move towards enemy
+        return ['move', rg.toward(self.location, closestEnemy)]
